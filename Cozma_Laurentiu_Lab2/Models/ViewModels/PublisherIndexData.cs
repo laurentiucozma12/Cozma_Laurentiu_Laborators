aef1350a0beb9df616bd4362dfc976186df3d0e2
Lau <laurentiucozma12@gmail.com>
2022-11-18 16:59:48 +0200

    ex4 terminat (1 eroare in Index.cshtml.cs din Publishers)

---
+++ Cozma_Laurentiu_Lab2/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Cozma_Laurentiu_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {      
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
